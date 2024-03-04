40dd350ecc2345efe8958d084cecdbc1fe9d88ed
Evgeni Glushko <sstawr@gmail.com>
2024-03-05 02:24:14 +0300

    Initial Commit

---
+++ Colorized/ColorizedApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorizedApp.swift
+//  Colorized
+//
+//  Created by Evgeni Glushko on 5.03.24.
+//
+
+import SwiftUI
+
+@main
+struct ColorizedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
